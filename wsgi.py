d588abcadc4338ff6ce05b94bfaf6cfdbd8d9d86
roczhang <1587839905@qq.com>
2021-06-21 01:18:18 +0800

    管理文件

---
+++ wsgi.py
@@ -0,0 +1,10 @@
+# Author : roczhang
+# date :   2021/6/21
+import os
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+from score import app
